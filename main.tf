18d5aa6a0019d92463f934ddf7811af9d84de45c
Sakshi9111 <chaysakshi911@gmail.com>
2023-04-23 10:11:25 +0545

    commit message

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-      region = "ap-south-1"
-    }
-  }
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 
