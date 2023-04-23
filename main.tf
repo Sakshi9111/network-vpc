621d23960a32d95f5712081030b3ba97c5e8bd6d
Sakshi9111 <chaysakshi911@gmail.com>
2023-04-23 10:07:22 +0545

    adding main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
 resource "aws_instance" "example" {
-  ami           =  "ami-0c768662cc797cd75" 
+  ami           = "ami-0c768662cc797cd75"
   instance_type = "t2.micro"
 }
